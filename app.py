7d94355c7182a1c4512dd9e288fdbdec346f0b03
Yoshnee Raveendran <yoshneer@spotify.com>
2024-07-13 16:53:02 -0400

    App.py to return index.html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Its yoshnee'
+	return render_template('index.html')
+    #return 'Hello World! Its yoshnee'
 
 if __name__ == '__main__':
     app.run()
